8dc9d4e66e4c1df271bc4d943cd8bb7597774012
James Hattersley <jameshattersley83@gmail.com>
2019-06-05 08:03:38 +0100

    created express server

---
+++ server.js
@@ -0,0 +1,12 @@
+// create express server
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("this is working");
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
